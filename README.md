ff9c37cc08020da8edff7976dce69387d32d0651
Eric L <126723001+elavallee13@users.noreply.github.com>
2023-04-19 13:25:12 -0500

    Error when commiting. Resolved issue

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Timed_Quiz_API02
-This is a 10 question quiz on some features of HTML, CSS and JavaScript
+CSS HTML and JavaScript Quiz
+This is a 10-question quiz designed to test your knowledge of CSS, HTML, and JavaScript. The quiz covers a range of topics from basic syntax to more advanced concepts.
+
+Getting Started
+To begin the quiz, simply click the "Start Quiz" button on the homepage. You will then be presented with a series of multiple-choice questions. Choose the answer that you believe is correct to move on to the next question.
+
+Once you have answered all 10 questions, you will be taken to a results page where you can see your score and a button that allows you to retake the quiz.
+
+Prerequisites
+In order to take this quiz, you should have a basic understanding of HTML, CSS, and JavaScript.
+This quiz is designed to be run in any modern web browser.
+
+Authors
+This quiz was created by [Eric Lavallee], a student at the UT Coding Bootcamp learn skills such as CSS, HTML, and JavaScript.
+
+Acknowledgments
+Thanks to the following resources, I was able to successfully complete this challenge:
+
+W3Schools
+MDN Web Docs
+Stack Overflow
+Peers
+Mentors
---
+++ script.js
@@ -0,0 +1,182 @@
+const questions = [
+  {
+    question: "What is the correct way to declare a JavaScript variable?",
+    choices: [
+      "variable x = 5;",
+      "var x = 5;",
+      "v x = 5;",
+      "None of the above"
+    ],
+    answer: "var x = 5;"
+  },
+  {
+    question: "What is the correct way to write an if statement in JavaScript?",
+    choices: [
+      "if i == 5 then",
+      "if i = 5 then",
+      "if (i == 5)",
+      "if i = 5",
+    ],
+    answer: "if (i == 5)"
+  },
+  {
+    question: "What is the correct way to write a for loop in JavaScript?",
+    choices: [
+      "for (i < 5; i++)",
+      "for (i = 0; i < 5; i++)",
+      "for (i <= 5)",
+      "for (i = 0; i <= 5)"
+    ],
+    answer: "for (i = 0; i < 5; i++)"
+  },
+  {
+    question: "Which of the following is not a semantic element in HTML?",
+    choices: [
+      "for (artical)",
+      "for (nav)",
+      "for (div)",
+      "for (footer)"
+    ],
+    answer: "for (div)"
+  },
+  {
+    question: "What is the default display property for block-level elements in CSS?",
+    choices: [
+      "for (inline)",
+      "for (block)",
+      "for (inline-block)",
+      "for (none)"
+    ],
+    answer: "for (block)"
+  },
+  {
+    question: "Which property in CSS is used to change the background color of an element?",
+    choices: [
+      "for (color)",
+      "for (background-color)",
+      "for (background0image)",
+      "for (none)"
+    ],
+    answer: "for (background-color)"
+  },
+  {
+    question: "What is the purpose of the <meta> tag in HTML?",
+    choices: [
+      "for (To specify the title of the web page)",
+      "for (To create a hyperlink to another web page)",
+      "for (To define the character encoding of the web page)",
+      "for (To add images to the web page)"
+    ],
+    answer: "for (To define the character encoding of the web page)"
+  },
+  {
+    question: "What is the difference between padding and margin in CSS?",
+    choices: [
+      "for (Padding is the space inside an element, while margin is the space outside the element.)",
+      "for (Padding is the space between an element and its border, while margin is the space inside the element.)",
+      "for (Padding and margin are the same thing in CSS.)",
+      "for (Padding is used for text formatting, while margin is used for page layout.)"
+    ],
+    answer: "for (Padding is the space inside an element, while margin is the space outside the element.)"
+  },
+  {
+    question: "What is the purpose of the href attribute in an <a> tag in HTM?L",
+    choices: [
+      "for (To specify the text content of the link)",
+      "for (To specify the URL that the link should go to)",
+      "for (To specify the color of the link)",
+      "for (To specify the font size of the link)"
+    ],
+    answer: "for (To specify the URL that the link should go to)"
+  },
+  {
+    question: "What is the difference between the <b> and <strong> tags in HTML?",
+    choices: [
+      "for (There is no difference, they both make text bold.)",
+      "for (<b> is used for aesthetic purposes, while <strong> is used for semantic purposes.)",
+      "for (<strong> is used for aesthetic purposes, while <b> is used for semantic purposes.)",
+      "for (<b> and <strong> are used interchangeably in HTML.)"
+    ],
+    answer: "for (<b> is used for aesthetic purposes, while <strong> is used for semantic purposes.)"
+  },
+  
+];
+
+let currentQuestion = 0;
+let score = 0;
+
+const quizContainer = document.querySelector("#quiz-container");
+const questionEl = document.querySelector("h2");
+const choicesEl = document.querySelector(".choices-container");
+// const nextBtn = document.querySelector("#next-btn");
+const endQuizContainer = document.querySelector("#quiz-end-container");
+
+function showQuestion() {
+  const question = questions[currentQuestion];
+  questionEl.textContent = question.question;
+  choicesEl.innerHTML = "";
+  question.choices.forEach(choice => {
+    const button = document.createElement("button");
+    button.classList.add("choice-btn");
+    button.textContent = choice;
+    button.addEventListener("click", () => {
+      if (button.textContent === question.answer) {
+        score++;
+      }
+      if (currentQuestion === questions.length - 1) {
+        endQuiz();
+      } else {
+        currentQuestion++;
+        showQuestion();
+      }
+    });
+    choicesEl.appendChild(button);
+  });
+}
+
+function endQuiz() {
+  quizContainer.style.display = "none";
+  endQuizContainer.insertAdjacentHTML('beforeend', `
+    <h2>You scored ${score} out of ${questions.length}.</h2>
+    <button id="back-btn">Start Over</button>
+  `);
+  const restartBtn = document.querySelector("#restart-btn");
+  // Insert directiv of case - - - remove cherie
+  
+  const backBtn = document.querySelector("#back-btn");
+  backBtn.addEventListener("click", function(){
+    window.location.href = "index.html";
+  });
+}
+
+showQuestion(); {
+}
+
+// quiz.js
+function showResults() {
+  // calculate the score and display it on the page
+  const score = calculateScore();
+  const scorePage = createScorePage(score);
+  document.body.innerHTML = scorePage;
+  
+  // add event listener to the restart button
+  const restartBtn = document.getElementById('restart-btn');
+  restartBtn.addEventListener('click', () => {
+    window.location.reload();
+  });
+}
+
+function calculateScore() {
+  // calculate the score based on the user's answers
+  // and return it
+}
+
+function createScorePage(score) {
+  // create a new HTML page to display the score
+  const html = `
+    <h1>Quiz Results</h1>
+    <p>Your score: ${score}</p>
+    <button id="restart-btn">Restart Quiz</button>
+  `;
+  return html;
+}
